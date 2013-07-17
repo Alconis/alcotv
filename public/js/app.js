2f45eed81b470e5fd2f9d1a9a3621377f719a0a8
Nicolas Demange <nicolas.demange@misys.com>
2013-07-17 11:01:01 +0200

    First handling of subscription to a game.

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,6 @@
 'use strict';
 
 var alcotv = angular.module('alcotv', ['firebase', 'ui.bootstrap'])
-	.value('fbURL', 'https://alcotv.firebaseio.com/alcotv')
-	.factory('Games', function(angularFireCollection, fbURL) {
-		return angularFireCollection(fbURL + '/games');
-	})
 	.config(function($routeProvider) {
 		$routeProvider.
 			when('/', {controller:'WelcomeCtrl', templateUrl:'partials/welcome.html'}).
@@ -12,4 +8,46 @@ var alcotv = angular.module('alcotv', ['firebase', 'ui.bootstrap'])
 			when('/tv', {controller:'TVCtrl', templateUrl:'partials/tv.html'}).
 			when('/player/:playerName', {controller:'PlayerCtrl', templateUrl:'partials/player.html'}).
 			otherwise({redirectTo:'/'});
-		});
+		})
+	.controller('MainCtrl', ['$scope', 'angularFire', function($scope, angularFire){
+		/*
+		 * GLOBAL SCOPE VARIABLES
+		 */
+
+		// Index the current game in the games
+		$scope.theGameID = 0;
+
+		// The game being played
+		$scope.theGame;
+
+		// The current player
+		$scope.thePlayer = undefined;
+
+		// When connected, the index of the current player
+		// in the Array of players.
+		$scope.thePlayerID = -1;
+
+		// true if the current player is presenter
+		$scope.isPresenter = false;
+
+		// true if we are the TV
+		$scope.isTV = false;
+
+		/*
+		 * Retrieve the game from firebase
+		 * and update $scope.theGame.
+		 */
+		$scope.working = "Retrieving Games.."
+		var fbUrl = "https://alcotv.firebaseio.com/games/" + $scope.theGameID;
+		var promise = new angularFire(fbUrl, $scope, 'theGame', {});
+		promise.then(
+			function success(){
+				$scope.working = undefined;
+			},
+			function error(){
+				$scope.working = "Unable to retrieve games."
+			});
+
+		
+
+	}]);
--- public/js/controllers/welcome-controller.js
+++ public/js/controllers/welcome-controller.js
@@ -4,14 +4,58 @@
 	tv.controller(
 		'WelcomeCtrl',
 		['$scope', 'angularFire', function($scope, angularFire){
-		
-			$scope.$watch('games', function(newValue, oldValue){
-				if(newValue && newValue.length > 0)
-					$scope.theGame = newValue[0];
-			});
-		
-			var fbUrl = "https://alcotv.firebaseio.com/games";
-			var promise = new angularFire(fbUrl, $scope, 'games', []);
+
+			$scope.thePlayerName = '';
+
+			$scope.enterAsPlayer = function() {
+				// If first player
+				if(!$scope.theGame.players){
+					$scope.theGame.players = [];
+				}
+
+				// If duplicated player name
+				ng.forEach($scope.theGame.players, function(value, key){
+					if(value.name == $scope.thePlayerName){
+						$scope.theGame.players.splice(value, 1);
+						return;
+					}
+				});
+				var newPlayer = {
+					name : $scope.thePlayerName,
+					status : 'ready',
+					score : 0
+				}
+				$scope.thePlayerID = $scope.theGame.players.push(newPlayer);
+				$scope.isPresenter = false;
+				$scope.isTV = false;
+
+				// Sync the player with firebase
+				var fbUrl = "https://alcotv.firebaseio.com/games/"
+				 				+ $scope.theGameID + "/players/"
+				 				+ $scope.thePlayerID;
+				var promise = new angularFire(fbUrl, $scope, 'thePlayer', {});
+			};
+
+			$scope.enterAsPresenter = function() {
+				$scope.theGame.parameters.presenter = $scope.thePlayer.name;
+				$scope.isPresenter = true;
+				$scope.isTV = false;
+
+				// If existing player name
+				ng.forEach($scope.theGame.players, function(value, key){
+					if(value.name == $scope.thePlayer.name){
+						$scope.theGame.players.splice(value, 1);
+						return;
+					}
+				});
+			};
+
+			$scope.enterAsTV = function() {
+				$scope.isPresenter = false;
+				$scope.isTV = true;
+			};
+
+
 		}]);
 
 })(angular, alcotv);
